3e40fb6f5bf786819dff349408684421f76f0a4f
johanr98 <jsromerod@unal.edu.co>
2016-10-17 20:43:36 -0500

    creation

---
+++ UtilExamples/src/Scanner/Example.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Scanner;
+
+import Arrays.*;
+
+/**
+ *
+ * @author johanr98 jsromerod@unal.edu.co
+ */
+public class Example {
+    
+}
